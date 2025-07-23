1f83d8633eb8fc0fa78b496cc4df8a1152ff0ed2
drHope099 <hopechach59@gmail.com>
2025-07-23 14:35:31 +0300

    like me

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-hope is me
+#halaaa
+
+hope is me and no one loves me as me except God
+
+let us pray
---
+++ appy.py
@@ -0,0 +1,12 @@
+first_name = "Hope"
+last_name = "Japhet"
+email = "hopejaphet963@gmail.com"
+phone_number = "+255 699 192 278"
+github_username = "drHope099"
+age = "20"
+
+print(f"Name: {first_name} {last_name}")
+print(f"Email: {email}")
+print(f"Phone Number: {phone_number}")
+print(f"GitHub Username: {github_username}")
+print(f"Age: {age}")
